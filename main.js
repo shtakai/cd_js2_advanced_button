151cf5f3227f9bb885b7187dcc1c4f02a7c00f20
Alyson Takai <shtakai@gmail.com>
2016-07-06 18:23:35 -0700

    assignment jquery

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 //
 console.log('test');
+$('button').on('click',function(e){
+  console.log('event',e);
+  if($('button').hasClass('blue')){
+    $('button').removeClass('blue');
+    $('button').addClass('red');
+
+  }else{
+    $('button').removeClass('red');
+    $('button').addClass('blue');
+  }
+});
+
+$('button').on('mouseenter', function(e){
+   console.log(e);
+   $('button').addClass('green');
+});
+
+$('button').on('mouseleave', function(e){
+   console.log(e);
+   $('button').removeClass('green');
+});
